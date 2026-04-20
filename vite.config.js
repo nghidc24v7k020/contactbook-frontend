2f2f22aa26fd001d2b77e341a473f149a2a898ed
nghidc24v7k020 <nghidc24v7k020@vlvh.ctu.edu.vn>
2026-04-20 12:48:51 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,22 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
-  server: {
-    port: 3001,
-  },
+    server: {
+        port: 3001,
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000/",
+                changeOrigin: true,
+            },
+        }
+    }
 })
