5b8984fe6f9d32522060b03dc0282702d1de2c8f
nghidc24v7k020 <nghidc24v7k020@vlvh.ctu.edu.vn>
2026-04-20 14:16:16 +0700

    Tao trang cap nhat lien he

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="page text-center mt-5">
+        <h1 class="display-4">404</h1>
+        <p>
+            Oops, không thể tìm thấy trang. Trở về
+            <router-link to="/">trang chủ.</router-link>
+        </p>
+    </div>
+</template>
+
+<style scoped>
+.page {
+    text-align: center;
+}
+
+h1 {
+    font-weight: bold;
+    color: #dc3545; 
+}
+</style>
